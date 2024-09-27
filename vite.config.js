bbe8e6286791598034fbbb98c2a4a1a823b13dea
Cresto Lima <crestolima67@gmail.com>
2024-09-27 12:36:14 +0530

    minor-bug-fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  esbuild: {
+    loader: 'jsx', // Treat .js files as JSX
+    include: /src\/.*\.[tj]sx?$/, // Include .js, .ts, .jsx, and .tsx files from src folder
+  },
+  server: {
+    host: true, // Expose on 0.0.0.0 for external access
+    port: 5173, // Optional: set the port explicitly
+  },
 })
